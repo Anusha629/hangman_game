c90c6a991f8a73aca54627ca4a804c87b13790fd
anusha <anushakizhakkeveedu@gmail.com>
2023-11-04 15:19:22 +0530

    GREEN: pick a random word and check conditions

--- hangman.py
+++ hangman.py
@@ -2,8 +2,23 @@ import random
 
 def get_random_word(wordlist="/usr/share/dict/words"):
     good_words = []
+    with open(wordlist) as f:
+        words = [x.strip() for x in f]
 
+    for word in words:
+            if not word.isalpha(): 
+                continue
+            if not word.islower():
+                continue
+            if len(word) < 5: 
+                continue
+            good_words.append(word)
 
-
+    secret_word = random.choice(good_words)
     return secret_word
+    
+
+
+
+
 
--- test_hangman.py
+++ test_hangman.py
@@ -1,5 +1,5 @@
 import os
-from hangman import * 
+import hangman
 
 def test_random_word_lowercase():
     fname = "/tmp/sample_wordlist"
@@ -7,8 +7,32 @@ def test_random_word_lowercase():
         f.writelines(["Grape\n", "apple\n", "Mango\n"])
         
     for _ in range(100):
-        assert get_random_word(fname) == "apple"
+        assert hangman.get_random_word(fname) == "apple"
 
     os.unlink(fname)
+
+def test_random_word_no_punctuation():
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["pineapple\n", "mango's\n", '"beryl"'])
+
+    for _ in range(100):
+        assert hangman.get_random_word(fname) == "pineapple"
+    
+    os.unlink(fname)
+
+def test_random_word_min_length_5():
+    fname = "/tmp/sample_wordlist"
+    with open(fname, "w") as f:
+        f.writelines(["pineapple\n", "ape\n", 'dog\n', 'bear\n'])
+
+    for _ in range(100):
+        assert hangman.get_random_word(fname) == "pineapple"
+        
+    os.unlink(fname)
+
+def test_random_word_no_repeated_words():
+    words = {hangman.get_random_word() for _ in range(10)}
+    assert len(words) == 10
 
 
